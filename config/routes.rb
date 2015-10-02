aa6cf74e20cc88688b644fd5d8e7b9f6abc6d680
Pablo Sanabria <pablo.s.q1.4.1991@gmail.com>
2015-10-02 12:08:46 -0400

    Added root page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  root 'static_pages#index'
+
   resources :contacts
   resources :work_posts
   # The priority is based upon order of creation: first created -> highest priority.
